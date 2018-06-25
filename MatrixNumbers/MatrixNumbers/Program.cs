c8f970ab03f74697cd4e0326ed2206141a17d064
Sait ORHAN <saitorhan@outlook.com>
2018-06-25 16:59:05 +0300

    sayılar kayıyor

---
+++ MatrixNumbers/MatrixNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = Enum.GetNames(typeof(ConsoleColor));
+            int next;
+            Random random = new Random();
+            Console.BackgroundColor = ConsoleColor.Black;
+            
+            while (true)
+            {
+                next = random.Next(0, colors.Length);
+                Console.ForegroundColor = (ConsoleColor) next;
+                Console.Write(random.Next(0,100));
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
